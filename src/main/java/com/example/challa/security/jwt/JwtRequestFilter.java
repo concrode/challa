74708c4ac2a63b8b7d10f9b9582388a57d295b67
concrode <captainshenhua@gmail.com>
2021-02-04 10:50:51 +1100

    updates

--- src/main/java/com/example/challa/security/jwt/JwtRequestFilter.java
+++ src/main/java/com/example/challa/security/jwt/JwtRequestFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtRequestFilter extends OncePerRequestFilter {
+    
     @Autowired
     private JwtUtils jwtUtils;
 
